695b76d86a0c4d53a608b934a5624c39c39288c7
Nicholas Guerra <nick@scriptthread.com>
2017-11-13 19:55:34 -0800

    added follow

--- instaLike.js
+++ instaLike.js
@@ -1,23 +1,41 @@
 function getHeartElement() { 
-    var knownHeartElementNames = ["coreSpriteHeartOpen", "coreSpriteLikeHeartOpen"]; 
+    // CSS class names to select heart
+    var knownHeartElementNames = [
+        "coreSpriteHeartOpen",
+        "coreSpriteLikeHeartOpen",
+        "coreSpriteHeartFull"
+    ]; 
+    // find the element that by trying to select it via querySelector
     for (i = 0; i < knownHeartElementNames.length; i++) { 
         var heartElement = document.querySelector('.' + knownHeartElementNames[i]); 
         if (heartElement != undefined) { 
             break; 
         } 
-    } return heartElement; 
+    }
+    return heartElement; 
 }
+
 function doLike() { 
+
     var likeElement = getHeartElement(); 
     var nextElement = document.querySelector('.coreSpriteRightPaginationArrow');
-    likeCount++; 
-    console.log('Liked ' + likeCount); 
     var nextTime = Math.random() * (14000 - 4000) + 4000; 
+    
+
     likeElement.click(); 
+    likeCount++; 
+    console.log('Liked ' + likeCount);
+
+    // click follow - "_njrw0" is follow button class
     document.querySelector('._njrw0').click();
+
+    // wait one second and click next
     setTimeout(function() {
         nextElement.click();
-    }, 1000); 
+    }, 1000);
+    
+    // If likeCount is less than 30 continue the loop
+    // If likeCount > 30 reset like counter and wait 5 min before continuing.
     if (likeCount < 30) { 
         setTimeout(doLike, nextTime); 
     } else {
@@ -28,4 +46,5 @@ function doLike() {
         }, 300000); 
     } 
 }
+// start program
 var likeCount = 0; doLike();
